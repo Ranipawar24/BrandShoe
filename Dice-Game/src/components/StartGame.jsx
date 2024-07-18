0e54cfb18b53ba4bf789805a8f4e1cb797f8606e
Rani Pawar <pawar.rani241996@gmail.com>
2024-07-18 13:47:32 +0530

    img

--- Dice-Game/src/components/StartGame.jsx
+++ Dice-Game/src/components/StartGame.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const StartGame = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <img src="/images/dices 1.png" />
+    </Container>
+  );
 };
 
 export default StartGame;
+
+const Container = styled.div`
+  max-width: 1180px;
+`;
