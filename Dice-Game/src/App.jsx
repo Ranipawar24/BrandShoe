3973ffa387f02b6c6e6c082bacb8ec89e9c5fb17
Rani Pawar <pawar.rani241996@gmail.com>
2024-07-18 13:36:30 +0530

    changes

--- Dice-Game/src/App.jsx
+++ Dice-Game/src/App.jsx
@@ -1,14 +1,15 @@
 import styled from "styled-components";
+import StartGame from "./components/StartGame";
 
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  padding: 10px;
-`;
+// const Button = styled.button`
+//   background-color: black;
+//   color: white;
+//   padding: 10px;
+// `;
 function App() {
   return (
     <>
-      <Button>This is styled components</Button>
+      <StartGame/>
     </>
   );
 }
